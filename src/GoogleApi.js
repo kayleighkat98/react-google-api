b3e269e436b2f5f1213396999420894d1d61d7ac
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-27 13:45:14 -0500

    linked api fetch properly

--- src/GoogleApi.js
+++ src/GoogleApi.js
@@ -4,17 +4,36 @@ import React, { Component } from 'react'
 
 class GoogleApi extends Component {
 
-    constructor(props) {
-        super(props)
-        this.state = {
-            
-        };
+   state = {
+        loading: false,
+        books: [],
+        id:[]//added state to referece book
+    };
+    
+    componentDidMount(){
+        this.setState({loading:true});
+        fetch('https://www.googleapis.com/books/v1/volumes?q=LostHero/')
+        .then(res => res.json())
+        .then(books => this.setState({
+            books: books.items,
+            // books: [books.items.title],
+            // books,
+            loading:false
+        }))
+        // console.log(books)
+    
     }
-
     render() {
+        // {console.log(this.state.books);
+        if (this.state.loading){
+            return <div>Loading...</div>;
+        }
+    const books =this.state.books.map(book=> <li key={book.id}>{book.volumeInfo.title}' '{book.volumeInfo.authors}</li>);
         return(
             <div className='container'>
-               <p>test</p>
+
+                <ul>{books}</ul>
+
             </div>
         );
     }
