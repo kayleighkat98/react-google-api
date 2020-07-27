0974d38d70c57b5ed35fbe80f79130e1d4b98b1b
Julia Bugayev <jbugayev18@gmail.com>
2020-07-27 12:50:25 -0700

    Slow and Steady Progress

--- src/GoogleApi.js
+++ src/GoogleApi.js
@@ -7,7 +7,8 @@ class GoogleApi extends Component {
    state = {
         loading: false,
         books: [],
-        id:[]//added state to referece book
+        id:[],//added state to referece book
+        search: {},
     };
     
     componentDidMount(){
@@ -23,18 +24,33 @@ class GoogleApi extends Component {
         // console.log(books)
     
     }
+
+    searchChanged(search) {
+        this.setState({
+            search
+        }); 
+    }
+
     render() {
+        // console.log(this.state.search)
         // {console.log(this.state.books);
         if (this.state.loading){
             return <div>Loading...</div>;
         }
-    const books =this.state.books.map(book=> <li key={book.id}>{book.volumeInfo.title}' '{book.volumeInfo.authors}</li>);
+    const books =this.state.books.map(book => <li key={book.id}>{book.volumeInfo.title}' '{book.volumeInfo.authors}</li>);
         return(
+            <form className="container__form">
             <div className='container'>
+            <label htmlFor="search">Search:</label>
+          <input type="text" name="search" id="search" 
+          placeholder="Title/Author" 
+        //   value = {this.state.search} 
+          onChange = {e => this.searchChanged(e.target.value)}/>
 
                 <ul>{books}</ul>
 
             </div>
+            </form>
         );
     }
 }
