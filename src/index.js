3221e078168dd4c985c319d1f034c8850266a2da
Julia Bugayev <jbugayev18@gmail.com>
2020-07-27 09:46:19 -0700

    Work In Progress

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
+fetch('https://www.googleapis.com/books/v1/volumes?q=LostHero')
+
 ReactDOM.render(<App />, document.getElementById('root'));
