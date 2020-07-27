1b7db9bcc3503599991be91b2b7ddfc336e5b096
Kayleigh Young <64609499+kayleighkat98@users.noreply.github.com>
2020-07-27 10:38:36 -0500

    added test

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+//import './';
+import GoogleApi from './GoogleApi';
+
+class App extends Component {
+
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+      
+  //   };
+  // }
+
+  render() {
+    return(
+      <div className='container'>
+         <GoogleApi/>
+      </div>
+    );
+  }
+}
+
+export default App
---
+++ src/GoogleApi.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+//import './';
+//import  from './';
+
+class GoogleApi extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        };
+    }
+
+    render() {
+        return(
+            <div className='container'>
+               <p>test</p>
+            </div>
+        );
+    }
+}
+
+export default GoogleApi;
